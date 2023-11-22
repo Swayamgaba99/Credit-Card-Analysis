72cbe631ed32d90abab4ff9c2f9d8113df30730b
Swayam Gaba <85446480+Swayamgaba99@users.noreply.github.com>
2023-11-22 23:07:40 +0530

    Add files via upload

---
+++ Credit_Card_Analysis.cpp
@@ -0,0 +1,21 @@
+//Valid Card With Comapny Name
+#include<iostream>
+using namespace std
+
+int get_size(int n){
+    string_to_num=to_string(num);
+    return string_to_num.length();
+}
+int sum_of_odd_place(int n){
+    int sum=0;
+    str=to_string(n);
+    for(int i=get_size(str)-1;i>=0;i-=2){
+        sum+=n[i]-'0';
+    }
+}
+bool is_valid(long int n)(
+    return(get_size(n)>=13 &&  get_size(n)<=16)
+)
+int main(){
+    cout<<sum_of_odd_place(300);
+}
